8b929dc20f4cb5f383cde1875985455271c46eb6
huimintoh <hmintoh@gmail.com>
2019-04-09 19:03:24 +0800

    feat(Home): add content for summary. links to /coffee and /blooms disabled atm

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/prototype_dark.png";
+import bloomsLink from "../assets/images/blooms.png";
+import coffeeLink from "../assets/images/coffee.png";
 import "./home.css";
 
 class Home extends Component {
   render() {
     return (
-      <div id="home">
-        <h2>HOME</h2>
-        pending implementation
-      </div>
+      <section id="home">
+        <div className="homeGrid">
+          <div className="homeGrid__logo">
+            <img src={logo} alt="prototype" className="logo--large" />
+          </div>
+          <div className="homeGrid__summary">
+            <p>
+              To prototype is to materialise an idea, no matter how primitive
+              the result might seem. That very act takes us on a path of
+              continual refinement of what it could be. With Prototype, we want
+              to embrace abstract possibilities in a spirit of fearless enquiry.
+            </p>
+            <p>
+              The community revitalises us. What you see today is the result of
+              our shared journey; to elevate an existing experience and recreate
+              a memory so it runs deeper than before. After all, what is the
+              beauty of creating if it cannot be known and shared with?
+            </p>
+            <p>
+              So we welcome you to Prototype - where ideas that have been
+              brewing can finally bloom in full.
+            </p>
+          </div>
+          <div className="homeGrid__links">
+            <Link to="/coffee">
+              <img
+                src={coffeeLink}
+                alt="coffee-link"
+                className="logo--standard"
+              />
+            </Link>
+            <Link to="/blooms">
+              <img
+                src={bloomsLink}
+                alt="blooms-link"
+                className="logo--standard"
+              />
+            </Link>
+          </div>
+        </div>
+      </section>
     );
   }
 }
