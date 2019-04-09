438552f490cff536c55fc190e6a9b9bad897d833
huimintoh <hmintoh@gmail.com>
2019-04-09 17:27:09 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### prototypes
+
+website for prototypes.sg
+
+© 2019 prototpes holdings All Rights Reserved
---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./app.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">prototypes.sg</header>
+      </div>
+    );
+  }
+}
+
+export default App;
