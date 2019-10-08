7c41203b460c7ade514ced248eceb29db6a3f413
huimintoh <hmintoh@gmail.com>
2019-10-08 17:17:25 +0800

    feat(Blooms): add placeholder content

--- src/views/blooms.jsx
+++ src/views/blooms.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import chevronLeft from "../assets/icons/chevron-left.svg";
 import "./blooms.scss";
 
 class Blooms extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
     return (
       <div id="blooms">
-        <h2>BLOOMS</h2>
-        pending implementation
+        <div class="animated rotateInDownLeft slower">
+          <Link to="/" class="back-button ">
+            <img src={chevronLeft} alt="back-button" width="20" height="20" />
+          </Link>
+        </div>
+        <div class="content__summary animated fadeIn slower">
+          <p class="content__summary-intro">
+            coming soon, please check back shortly.
+          </p>
+        </div>
       </div>
     );
   }
