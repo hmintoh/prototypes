ecd72fbb60eef9639364a568dc0b5c0da486521c
hmintoh <hmintoh@gmail.com>
2020-01-03 09:00:53 +0800

    refactor(Footer): use Date() object to get current year

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
-  return <footer>© 2019 prototype holdings. all rights reserved.</footer>;
+  const currentYear = new Date().getFullYear();
+
+  return <footer>© {currentYear} prototype holdings. all rights reserved.</footer>;
 };
 
 export default Footer;
