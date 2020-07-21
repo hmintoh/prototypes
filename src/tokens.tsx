79dedb6f9885fc39253010da869644c3056fcd59
hmintoh <hmintoh@gmail.com>
2020-07-21 22:30:39 +0800

    styles(tokens): refactor tokens from .scss to .tsx

---
+++ src/tokens.tsx
@@ -0,0 +1,33 @@
+export const tokens = {
+  breakpoint: {
+    mobile: "620px",
+    tablet: "768px",
+    desktop: "1024px",
+  },
+  color: {
+    blue: "#262f3e",
+    maize: "#c6a351",
+    natural: {
+      light: "#f6f4f3",
+      dark: "#bdbdae",
+    },
+    pink: {
+      light: "#c6ab9c",
+      dark: "#a66454",
+    },
+    system: {
+      white: "#f6f4f3",
+      black: "#292e3d",
+      green: "#71ac50",
+      yellow: "#eba335",
+      red: "#f44336",
+    },
+  },
+  spacing: {
+    xxxxs: "",
+    s: "0.875rem",
+    m: "1rem",
+    l: "1.25rem" /**  l : 1.25rem / 20px  */,
+    xl: "1.5rem" /** xl: 1.5rem / 32px */,
+  },
+};
