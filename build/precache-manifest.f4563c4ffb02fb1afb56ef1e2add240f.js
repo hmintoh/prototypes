a634b49bf2ec568f437527d27e37d96c04a5c634
hmintoh <hmintoh@gmail.com>
2021-02-26 14:17:30 +0800

    refactor(views/coffee): update description

---
+++ build/precache-manifest.f4563c4ffb02fb1afb56ef1e2add240f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae3101ff32c1866f3469bcf7e4baae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf8fe4b9a198e6f3df5e",
+    "url": "/static/css/2.c98225b9.chunk.css"
+  },
+  {
+    "revision": "212ded05320e0608d03a",
+    "url": "/static/css/main.19a40b73.chunk.css"
+  },
+  {
+    "revision": "cf8fe4b9a198e6f3df5e",
+    "url": "/static/js/2.4014cfee.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.4014cfee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721ba90b8808710ac8a7",
+    "url": "/static/js/3.37646986.chunk.js"
+  },
+  {
+    "revision": "a954dbef8e7d0a0cc774",
+    "url": "/static/js/4.2572c0ae.chunk.js"
+  },
+  {
+    "revision": "06af3e54beaa935e31ad",
+    "url": "/static/js/5.ab304149.chunk.js"
+  },
+  {
+    "revision": "212ded05320e0608d03a",
+    "url": "/static/js/main.527bff0b.chunk.js"
+  },
+  {
+    "revision": "e457d44f479df19c9dcb",
+    "url": "/static/js/runtime-main.c5f51598.js"
+  },
+  {
+    "revision": "b5918b760a8a2edc8128ba0ebb618a4f",
+    "url": "/static/media/Ambasador.b5918b76.ttf"
+  },
+  {
+    "revision": "f46f486edefeba66ac4b538dd593d656",
+    "url": "/static/media/Quicksand-Regular.f46f486e.ttf"
+  },
+  {
+    "revision": "3c4f3b3dd62ad09132ea8c624181e979",
+    "url": "/static/media/Tango.3c4f3b3d.ttf"
+  },
+  {
+    "revision": "7572c4cee5323ae69e848892a919282e",
+    "url": "/static/media/bg_cloud_blue.7572c4ce.jpg"
+  },
+  {
+    "revision": "d97959aee26b0f5dacebbeec0886f628",
+    "url": "/static/media/bg_cloud_brown_2.d97959ae.jpg"
+  },
+  {
+    "revision": "ea95abf3ab64d81a48eb18887ab8e9c0",
+    "url": "/static/media/proto_blue.ea95abf3.png"
+  }
+]);
--- src/views/coffeepage/index.tsx
+++ src/views/coffeepage/index.tsx
@@ -11,19 +11,26 @@ const CoffeePage: FC<{}> = () => {
       <ContentIntro>
         <h1>prototype Coffee</h1>
         <img src={coffeeLogo} alt="prototoype coffee logo" />
-        <p>
+        {/* <p>
           A good cup of coffee speaks of the collective effort and success of
           the many hands that went into delivering the coffee into your hands.
           From the farmers to the roasters to the baristas, they all play in
           part in preserving the coffee's character and form its story.
-        </p>
+        </p> */}
+        <p>Prototype coffee is a verb, not a noun.</p>
       </ContentIntro>
-
       <p>
-        With prototype coffee, our goal is to share these stories with the
-        community. We aim to provide quality coffee experience by constantly
-        working towards improvements in our technique and service. For
-        enquiries, please get in touch at{" "}
+        To prototype is to enter the flux of materializing ideas, unbound by
+        expectation or result.
+      </p>
+      <p>
+        The cold brew you are holding has been steeped in collective critique
+        and bottled with thoughtful experimentation. It reflects the taste of
+        our small but growing community — have a sip and find yourself warmly
+        welcomed in the process.
+      </p>
+      <p>
+        For enquiries, please get in touch at{" "}
         <a href="mailto:coffee@prototypes.sg?subject=Inquiry on coffee cart services">
           coffee@prototypes.sg
         </a>
